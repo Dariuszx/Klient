3f92b1efcdac1084636850a1732fbcf0acd1db93
dariuszx <marazmad.01@gmail.com>
2015-01-26 01:06:43 +0100

    Zmiany

--- Klient/urls.py
+++ Klient/urls.py
@@ -1,12 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from views import login, index, logout, my_ideas, show_idea, show_thread, new_note
+from views import login, index, logout, my_ideas, show_idea, show_thread, new_note, new_thread, show_note, delete_note, edit_note
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Klient.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^login$', login),
     url(r'^$', index),
     url(r'^logout$', logout),
@@ -14,4 +10,8 @@ urlpatterns = patterns('',
     url(r'^show/idea/(?P<idea_id>\d+)', show_idea),
     url(r'^show/thread/(?P<thread_id>\d+)', show_thread),
     url(r'^new/note/(?P<thread_id>\d+)', new_note),
+    url(r'^new/thread/(?P<idea_id>\d+)', new_thread),
+    url(r'^show/note/(?P<note_id>\d+)', show_note),
+    url(r'^delete/note/(?P<note_id>\d+)', delete_note),
+    url(r'^edit/note/(?P<note_id>\d+)', edit_note),
 )
--- Klient/views.py
+++ Klient/views.py
@@ -75,6 +75,7 @@ def show_idea(request, idea_id):
 
     context, template = user_accout_template(request.session['login'])
     context.push({"option" : "threads"})
+    context.push({"idea_id" : idea_id})
 
     headers = {"Content-Type": "text/plain"}
     path = paths.get_idea_threads + str(idea_id)
@@ -119,8 +120,6 @@ def new_note(request, thread_id):
 
         r = requests.post(path, data=data)
 
-        print r.text
-
         if r.status_code == 200:
             return HttpResponseRedirect("/show/thread/"+str(thread_id))
 
@@ -130,4 +129,75 @@ def new_note(request, thread_id):
 
     return HttpResponse(template.render(context))
 
+def new_thread(request, idea_id):
+    if not tools.is_logged(request):
+        return HttpResponseRedirect("/login")
 
+    if request.method == "POST" and request.POST.dict().has_key('content'):
+        dic = request.POST.dict()
+        data = {"user_id" : request.session['user_id'], "idea_id" : idea_id, "overview" : dic['content']}
+
+        path = paths.new_thread
+
+        r = requests.post(path, data=data)
+
+        print r.text
+
+        if r.status_code == 200:
+            return HttpResponseRedirect("/show/idea/"+str(idea_id))
+
+    context, template = user_accout_template(request.session['login'])
+    context.push({"option" : "new_note"})
+
+    return HttpResponse(template.render(context))
+
+def show_note(request, note_id):
+    if not tools.is_logged(request):
+        return HttpResponseRedirect("/login")
+
+    path = paths.get_note + str(note_id)
+    result = requests.get(path)
+
+    context, template = user_accout_template(request.session['login'])
+    context.push({"option" : "show_note"})
+    context.push({"note_id" : note_id })
+
+    if result.status_code == 200:
+        data = json.loads(result.text)
+        context.push({"note" : data[0]})
+
+    return HttpResponse(template.render(context))
+
+def delete_note(request, note_id):
+    if not tools.is_logged(request):
+        return HttpResponseRedirect("/login")
+
+    data = json.loads(requests.get(paths.get_note+str(note_id)).text)
+
+    path = paths.delete_note + str(note_id)
+    result = requests.delete(path)
+
+    if result.status_code == 200:
+        return HttpResponseRedirect("/show/thread/"+str(data[0]['thread_id']))
+
+    return HttpResponseRedirect("/")
+
+def edit_note(request, note_id):
+    if not tools.is_logged(request):
+        return HttpResponseRedirect("/login")
+
+    if request.method == "POST":
+        dict = request.POST.dict()
+
+        path = paths.edit_note
+        requests.put(path, data={"note_id" : note_id, "content" : dict['content']})
+
+    path = paths.get_note + note_id
+    result = requests.get(path)
+
+    context, template = user_accout_template(request.session['login'])
+    context.push({"option" : "new_note"})
+    data = json.loads(result.text)
+    context.push({ "content" : data[0]['text'] })
+
+    return HttpResponse(template.render(context))
